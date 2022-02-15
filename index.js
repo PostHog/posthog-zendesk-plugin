5108298d2388e51f273be18a72f30f4e3edd1d35
Yakko Majuri <38760734+yakkomajuri@users.noreply.github.com>
2022-02-15 08:48:36 +0000

    code review

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// <TODO: your plugin code here - you can base it on the code below, but you don't have to>
-
-// Plugin method that runs on plugin load
 export async function setupPlugin({ config, global, cache }) {
   console.info(`Setting up the plugin`);
   let baseUrl = `https://${config.hostName}.zendesk.com/`;
@@ -27,44 +24,11 @@ export async function setupPlugin({ config, global, cache }) {
   );
 
   if (!statusOk(authenticationResponse)) {
-    throw new Error(`Unable to access zendesk api's for ${config.hostName}`);
+    throw new Error(`Unable to access Zendesk API's for ${config.hostName}`);
   } else {
     console.info(
-      `Inetail setup with zendesk for ${config.hostName} successful`
+      `Initial setup with zendesk for ${config.hostName} successful`
     );
-  }
-}
-
-function isEmail(email) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
-}
-
-function statusOk(res) {
-  return String(res.status)[0] === "2";
-}
-
-async function fetchWithRetry(
-  url,
-  options = {},
-  method = "GET",
-  isRetry = false
-) {
-  try {
-    const res = await fetch(url, { method: method, ...options });
-    return res;
-  } catch {
-    if (isRetry) {
-      throw new Error(`${method} request to ${url} failed.`);
-    }
-    const res = await fetchWithRetry(
-      url,
-      options,
-      (method = method),
-      (isRetry = true)
-    );
-    return res;
   }
 }
 
@@ -109,7 +73,7 @@ async function fetchUserIdentity(requesterId, global, storage) {
 
 async function fetchAllTickets(global, storage, cache) {
   let allTickets = [null];
-  index = 1;
+  index = 1; // let index
   while (allTickets.length > 0) {
     const finalUrl = `${global.baseTicketUrl}&page=${index}`;
 
@@ -117,8 +81,9 @@ async function fetchAllTickets(global, storage, cache) {
     index += 1;
 
     allTickets = await allTicketData.json();
-    allTickets = await allTickets.tickets;
+    allTickets = await allTickets.tickets; // no await needed here
 
+    // nit: this isn't necessary. the loop will not do anything if allTickets.length === 0
     if (allTickets.length === 0) {
       break;
     }
@@ -128,6 +93,7 @@ async function fetchAllTickets(global, storage, cache) {
     for (let ticket of allTickets) {
       const customerRecordExists = await storage.get(ticket.id);
 
+      // why break and not continue?
       if (!customerRecordExists) {
         await storage.set(ticket.id, true);
       } else {
@@ -140,6 +106,7 @@ async function fetchAllTickets(global, storage, cache) {
         storage
       );
 
+      // nit: let's use camelCase
       const ticket_object_to_capture = {
         ticketId: ticket.id,
         created_at: ticket.created_at,
@@ -171,22 +138,31 @@ async function fetchAllTickets(global, storage, cache) {
 }
 
 export async function onEvent(event, { config, global, storage }) {
+
+  // let's not do this on every onEvent - better to do this validation in setupPlugin 
+  // and set triggeringEvents on global.
   let triggeringEvents = (config.triggeringEvents || "")
     .split(",")
     .map(function (value) {
       return value.trim();
     });
+
+
+  // nit: .includes is cleaner and is supported in plugin VMs
   if (triggeringEvents.indexOf(event.event) >= 0) {
     const email = getEmailFromEvent(event);
     if (email) {
       let emailDomainsToIgnore = (config.ignoredEmails || "")
         .split(",")
-        .map(function (value) {
+        .map(function (value) { // nit: here and above you can just do .map(v => v.trim())
           return value.trim();
         });
       if (emailDomainsToIgnore.indexOf(email.split("@")[1]) >= 0) {
         return;
       }
+
+      // let's do this in a job rather than call it directly from onEvent
+      // https://posthog.com/docs/plugins/build/reference#specifying-metrics#jobs-1
       await pushUserDataToZendesk(
         email,
         event.event,
@@ -218,6 +194,41 @@ function getEmailFromEvent(event) {
 }
 
 export async function runEveryMinute({ global, storage, cache }) {
+  // is latest_element_so_far ever used?
+  // nit: let's use camelCase instead of snake_case
   const latest_element_so_far = await cache.get("_latest_element_so_far");
   const allTickets = await fetchAllTickets(global, storage, cache);
+}
+
+function isEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+function statusOk(res) {
+  return String(res.status)[0] === "2";
+}
+
+async function fetchWithRetry(
+  url,
+  options = {},
+  method = "GET",
+  isRetry = false
+) {
+  try {
+    const res = await fetch(url, { method: method, ...options });
+    return res;
+  } catch {
+    if (isRetry) {
+      throw new Error(`${method} request to ${url} failed.`);
+    }
+    const res = await fetchWithRetry(
+      url,
+      options,
+      (method = method),
+      (isRetry = true)
+    );
+    return res;
+  }
 }
